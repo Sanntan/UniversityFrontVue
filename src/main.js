eda8fd57fd505ae33442e193e7aded7eb6dc3be6
Sanntan <gommaftegey04@mail.ru>
2024-03-04 16:01:47 +0300

    main coomit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import router from './modules/router.js';
+import store from './modules/store.js';
+import App from './App.vue';
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
