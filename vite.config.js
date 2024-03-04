67cf2e44a5e97274869eec6feb4224577844af2d
Sanntan <gommaftegey04@mail.ru>
2024-03-04 16:07:53 +0300

    main coomit

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,32 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const target = 'http://127.0.0.1:5050';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '^/summarization': {
+        target,
+        secure: false
+      },
+      '^/user': {
+        target,
+        secure: false
+      }
+    },
+    port: 5173
   }
 })
