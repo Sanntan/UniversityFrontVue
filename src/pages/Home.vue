5aa4d85026e15a693c3c2851163fdf260cfa9f57
neqw1te <ivansokol45@gmail.com>
2024-03-04 23:54:10 +0500

    04.03

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,15 +22,15 @@ async function onSummarizationClick() {
   <div class="mt-16">
     <form class="flex flex-col w-fit mx-auto gap-3" @submit.prevent="onSummarizationClick">
       <textarea
-        class="shadow-xl border border-slate-0 bg-slate-100 resize-none outline-none rounded-3xl px-3 py-2 placeholder:italic bg-gradient-to-br from-sky-200 via-sky-100 to-pink-200 mt-20 text-black font-bold bg-opacity-50"
-        rows="4"
+        class="shadow-xl resize-none outline-none rounded-3xl px-3 py-2 bg-gray-100 border mt-20 placeholder-gray-900 text-gray-900 font-sans placeholder:italic"
+        rows="6"
         cols="30"
-        placeholder="Введите текст"
+        placeholder="Вставьте файл"
         v-model="text"
       ></textarea>
 
       <input
-        class="cursor-pointer bg-white-600 rounded-3xl text-black py-2 border-2 border-black"
+        class="mt-5 mb-7 cursor-pointer bg-white-600 rounded-3xl text-black py-2 border-2 border-black"
         type="submit"
         value="Отправить"
       />
@@ -38,14 +38,14 @@ async function onSummarizationClick() {
 
     <form class="flex flex-col w-fit mx-auto gap-4" @submit.prevent="onSummarizationClick">
       <textarea
-        class="shadow-xl border border-slate-0 bg-slate-100 resize-none outline-none rounded-3xl px-3 py-2 placeholder:italic bg-gradient-to-br from-sky-200 via-sky-100 to-pink-200 mt-4 text-black font-bold bg-opacity-50"
-        rows="6"
+        class="shadow-xl resize-none outline-none rounded-3xl px-3 py-2 bg-gray-100 border placeholder-gray-900 text-gray-900 font-sans placeholder:italic"
+        rows="8"
         cols="100"
         placeholder="Обработанный текст"
         v-model="text"
       ></textarea>
 
-      <div class="flex flex-col gap-2 mt-4">
+      <div class="flex flex-col text-lg font-sans">
         <div class="flex justify-between items-center">
           <select v-model="summarizationMethod">
             <option value="abstractive">Абстрактивная</option>
@@ -55,7 +55,7 @@ async function onSummarizationClick() {
           <div class="flex items-center gap-4" v-if="summarizationMethod === 'extractive'">
             <p>Кол-во предложений:</p>
             <input
-              class="border border-slate-300 bg-slate-100 rounded-md px-2 py-2 w-24 text-center rounded-2xl"
+              class="border border-slate-300 bg-slate-100 px-2 py-2 w-24 text-center rounded-xl shadow-xl"
               type="number"
               value="3"
               min="3"
